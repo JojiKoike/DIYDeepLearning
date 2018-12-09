a567f22da828e23d6ea3818bc942486dd8c73a84
JojiKoike <joji.koike@gmail.com>
2018-12-09 14:38:04 +0900

    Add 2 layer perceptron sample (xor circuit)

--- perceptron/logic_circuit.py
+++ perceptron/logic_circuit.py
@@ -24,11 +24,19 @@ def or_gate(x1, x2):
     tmp = np.dot(X, W) + b
     return 1 if tmp > 0 else 0
 
-for i in range(2):
-    for j in range(2):
-        print("{0} and {1}  = {2}".format(i, j, and_gate(i, j)))
-        print("{0} nand {1} = {2}".format(i, j, nand_gate(i, j)))
-        print("{0} or {1} = {2}".format(i, j, or_gate(i, j)))
+def xor_gate(x1, x2):
+    s1 = nand_gate(x1, x2)
+    s2 = or_gate(x1, x2)
+    tmp = and_gate(s1, s2)
+    return 1 if tmp > 0 else 0
+
+if __name__ == '__main__':
+    for i in range(2):
+        for j in range(2):
+            print("{0} and {1}  = {2}".format(i, j, and_gate(i, j)))
+            print("{0} nand {1} = {2}".format(i, j, nand_gate(i, j)))
+            print("{0} or {1} = {2}".format(i, j, or_gate(i, j)))
+            print("{0} xor {1} = {2}".format(i, j, xor_gate(i, j)))
 
 
 
