1d4699ed200b7cd2c066262cf5709865bc4db1c9
JojiKoike <joji.koike@gmail.com>
2019-04-09 23:42:29 +0900

    Momentum

--- chapter6/gradient.py
+++ chapter6/gradient.py
@@ -1,5 +1,6 @@
-from typing import Dict
+from typing import Dict, Optional
 import numpy as np
+
 
 
 class SGD:
@@ -9,3 +10,20 @@ class SGD:
     def update(self, params: Dict[str, np.ndarray], grads: Dict[str, np.ndarray]):
         for key in params.keys():
             params[key] -= self.lr * grads[key]
+
+
+class Momentum:
+    def __init__(self, lr: float = 0.01, momentum: float = 0.9):
+        self.lr: float = lr
+        self.momentum: float = momentum
+        self.v: Optional[Dict[str, np.ndarray]] = None
+
+    def update(self, params: Dict[str, np.ndarray], grads: np.ndarray):
+        if self.v is None:
+            self.v = {}
+            for key, val in params:
+                self.v[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
